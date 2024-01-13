ed681c24e0332f05b70accbfb902171f16d0f737
justinelut <justinequartz@gmail.com>
2024-01-13 19:29:21 +0300

    Right layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,6 +47,29 @@ export default function Home() {
             </div>
           </SideCard>
         </div>
+        <div className="container">
+          <SideCard>
+            <h3 className="text-md font-extrabold mt-6">Github</h3>
+          </SideCard>
+
+          <SideCard>
+            <MyAvatar image="https://github.com/shadcn.png" />
+            <h3 className="text-md font-extrabold mt-6">
+              Frontend Web Developer
+            </h3>
+            <h2 className="text-2xl font-extrabold mt-4">Justine Gichana</h2>
+            <p>I am a Frontend developer, Optimist & Community builder.</p>
+            <div className="flex flex-row justify-between items-center mt-4 gap-4">
+              <div className="flex flex-1 flex-row items-center border p-2 rounded-lg justify-start">
+                <span>
+                  <MapPin size={16} className="mr-4" />
+                </span>{" "}
+                <span>Nakuru, Kenya</span>
+              </div>
+              <div className="border p-2 rounded-lg">Contact Me </div>
+            </div>
+          </SideCard>
+        </div>
       </div>
     </main>
   );
