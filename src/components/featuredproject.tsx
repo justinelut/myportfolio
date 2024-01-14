64cecfa217c4bf513adb90e1e29cddbd60c4adf7
justinelut <justinequartz@gmail.com>
2024-01-14 18:06:02 +0300

    fixing featured section projects

--- src/components/featuredproject.tsx
+++ src/components/featuredproject.tsx
@@ -15,7 +15,10 @@ export const FeaturedProject = () => {
 
   return (
     <Link href="/">
-      <ContentCard classContent="p-0" className="rounded-xl relative">
+      <ContentCard
+        classContent="p-0"
+        className="rounded-xl relative items-end overflow-hidden"
+      >
         <AspectRatio ratio={10 / 9} className="bg-muted">
           <Image
             src="/assets/featured.jpg"
@@ -24,8 +27,34 @@ export const FeaturedProject = () => {
             className="rounded-xl object-cover w-full h-full"
           />
         </AspectRatio>
-        <div className="absolute mt-0">
-          <span className="px-4 py-2">Category</span>
+
+        <div className="absolute flex items-end z-20 w-full inset-0 p-4 space-y-3 md:p-8 bg-gradient-to-t from-green-500 to-transparent">
+          <div className="flex flex-wrap gap-3">
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Categorydggggggggggggggggggggg
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Categoryddddddddddddddddddddd
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+            <span className="py-1 text-slate-800 text-sm bg-gray-200 px-4 rounded-lg">
+              Category
+            </span>
+          </div>
         </div>
       </ContentCard>
     </Link>
