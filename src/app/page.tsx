7ac8ed78c7953763526f81c9c7efe77bc85ec190
justinelut <justinequartz@gmail.com>
2024-01-15 19:53:42 +0300

    loading projects data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import { FeaturedProject } from "@/components/featuredproject";
 import { Projects } from "@/components/projects";
 import SmallSocial from "@/components/smallsocial";
 import { pb } from "@/lib/api";
+import { Loading } from "@/components/loading";
+import { Suspense } from "react";
 
 export default async function Home() {
   const projectdata = await pb.collection("Projects").getList(1, 4);
- 
+
   return (
     <section className="lg:col-span-2 md:col-span-2 flex gap-4">
       <div className="w-full flex flex-col gap-6">
@@ -17,15 +19,18 @@ export default async function Home() {
         <FeaturedProject />
         <div className="flex flex-col gap-3">
           <h3 className="text-sm mb-1 font-bold">@popular projects</h3>
-          {projectdata ?
-            projectdata.items.map((item, index) => (
+          
+          <Suspense fallback={<Loading />}>
+            {projectdata.items.map((item, index) => (
               <Projects
                 key={index}
                 githublink={item.githublink}
                 githubstars={item.githubstars}
                 projecttitle={item.projecttitle}
               />
-            )) : <Loading />}
+            ))}
+        
+          </Suspense>
         </div>
       </div>
 
