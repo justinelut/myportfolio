7bc2a48b026d8ed8f9af72c8b7f68794e37c7ac9
justinelut <justinequartz@gmail.com>
2024-01-17 17:20:21 +0300

    Portfolio

--- src/components/header/topnav.tsx
+++ src/components/header/topnav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TopNav() {
   return (
-    <div className="border-b h-20 bg-gray-100 dark:bg-slate-800 border-b-gray-100 w-[100%] sticky top-0 z-30">
+    <div className="border-b h-20 dark:bg-slate-950 w-[100%] bg-white sticky top-0 z-30">
       <div className="mx-auto container">my desktop navigation</div>
     </div>
   );
