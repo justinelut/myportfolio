805ad1d50c7a852a2193653a75ada6dd919c3a2b
justinelut <justinequartz@gmail.com>
2024-01-23 19:41:47 +0300

    removed duplicate

--- src/components/navigation copy.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function Navigation({ children }: Props) {
-  return <div className="md:sticky md:top-28 lg:top-28 md:left-0 lg:col-span-1 md:col-span-1">{children}</div>;
-}
