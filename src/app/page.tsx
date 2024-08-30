e6570fc4d8a5117263454d49a60869f3336ee838
justinelut <justinequartz@gmail.com>
2024-08-30 16:11:01 +0300

    updated the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ import FeaturedProjects from "./featured-projects/featured-projects";
 export const metadata: Metadata = {
   title: "Justine Gichana - Portfolio",
   description:
-    "Hi there! I'm Justine G, a passionate frontend developer specializing in React, Next.js, and Tailwind CSS. Check out my work and get to know my journey in web development.",
+    "Hi there! I'm Justine G, a passionate web developer specializing in React, Next.js, and Tailwind CSS. Check out my work and get to know my journey in web development.",
   keywords:
-    "Justine G, frontend developer, React, Next.js, Tailwind CSS, web development, portfolio",
+    "Justine G, web developer, React, Next.js, Tailwind CSS, web development, portfolio",
   openGraph: {
     images: [
       "https://api2.pixelayout.site/api/files/bs6ha7egdp060nv/mxbwu49exadjgpm/portrait_1_tPet10eAvs.webp?token=",
